039167b034784db0881a4c709bf972e7b43feef3
cbo31 <cbodev31@gmail.com>
2026-06-16 16:40:27 +0200

    Add: requirements.txt to install dependancys on different machine. Keep out Mistral AI to test simple route

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI
-from mistralai import Mistral
-from dotenv import load_dotenv
+# from mistralai import Mistral
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
-api_key = os.getenv("MISTRAL_API_KEY")
-client = Mistral(api_key=api_key)
+# api_key = os.getenv("MISTRAL_API_KEY")
+# client = Mistral(api_key=api_key)
 
 app = FastAPI()
 
-@app.post("/chat")
-def chat(message: dict):
-    response = client.chat.complete(
-        model="mi stral-small-2503",
-        messages=[
-            {"role": "???", "content": message["message"]}
-        ]
-    )
-    return {"response": response.choices[0].message.content}
+# @app.post("/chat")
+# def chat(message: dict):
+#     response = client.chat.complete(
+#         model="mi stral-small-2503",
+#         messages=[
+#             {"role": "user", "content": message["message"]}
+#         ]
+#     )
+#     return {"response": response.choices[0].message.content}
 
+@app.get("/test")
+def test():
+    return {"response": "Hello from backend"}
